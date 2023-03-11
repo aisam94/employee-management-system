44ecc34f9109c519ff86e30647abe7af001146b3
Aisamuddin <aisam0094@gmail.com>
2023-03-11 13:58:20 +0800

    decouple the frontend

--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,21 @@ app.use("/api/roles", rolesRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("/", (req, res) => {
+  res.send("API is running ...");
+});
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running ...");
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running ...");
+//   });
+// }
 
 app.listen(port, () => {
   //perform database connection when server starts
--- frontend/src/components/sidebar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const sidebar = () => {
-  return <div>this is a sidebar</div>;
-};
-
-export default sidebar;
