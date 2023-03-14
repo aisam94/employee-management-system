a52e8a1aab709cbbb1adf71805552aec07f255c7
Aisamuddin <aisam0094@gmail.com>
2023-03-14 10:22:36 +0800

    s

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 const corsOption = {
   origin: process.env.CLIENT_URL,
-  // origin: '*',
 }
 app.use(cors(corsOption));
 app.use(express.json());
@@ -28,13 +27,17 @@ app.use("/api/roles", rolesRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+app.get("/api", cors(corsOption), (req, res) => {
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send("API is running ...");
+});
 
-// app.get("/", cors(corsOption), (req, res) => {
 app.get("/", (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  // res.setHeader('Content-Type', 'text/html');
+  // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.set('Access-Control-Allow-Origin', '*');
   res.send("API is running ...");
 });
 
